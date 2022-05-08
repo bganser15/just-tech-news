a40245eac9c4b555155464abf2fc8645eede083a
Brittany Ganser <15brganser@gmail.com>
2022-05-07 21:47:11 -0600

    finished homepage to display all routes

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,7 +1,47 @@
 const router = require("express").Router();
+const sequelize = require("../config/connection");
+const { Post, User, Comment } = require("../models");
+
+//Get homepage with all posts using sequelize models
 
 router.get("/", (req, res) => {
-  res.render("homepage");
+  Post.findAll({
+    attributes: [
+      "id",
+      "post_url",
+      "title",
+      "created_at",
+      [
+        sequelize.literal(
+          "(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)"
+        ),
+        "vote_count",
+      ],
+    ],
+    include: [
+      {
+        model: Comment,
+        attributes: ["id", "comment_text", "post_id", "user_id", "created_at"],
+        include: {
+          model: User,
+          attributes: ["username"],
+        },
+      },
+      {
+        model: User,
+        attributes: ["username"],
+      },
+    ],
+  })
+    .then((dbPostData) => {
+      //creates an array of all the posts and serializes the data with plain: true
+      const posts = dbPostData.map((post) => post.get({ plain: true }));
+      res.render("homepage", { posts });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 module.exports = router;
